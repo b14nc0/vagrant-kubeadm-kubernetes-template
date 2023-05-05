fbe18ee9018c87f3dbc0b1d002be2978c55fa84d
b14nc0 <blanco88@gmail.com>
2023-05-05 09:23:28 +0200

    new file:   Vagrantfile

---
+++ Vagrantfile
@@ -0,0 +1,43 @@
+NodeCount=2
+MasterCount=1
+IP_NW="10.0.0."
+IP_START=10
+
+Vagrant.configure("2") do |config|
+    config.vm.provision "shell", inline: <<-SHELL
+        apt-get update -y
+        echo "$IP_NW$((IP_START))  master-node" >> /etc/hosts
+        echo "$IP_NW$((IP_START+1))  worker-node01" >> /etc/hosts
+        echo "$IP_NW$((IP_START+2))  worker-node02" >> /etc/hosts
+    SHELL
+    config.vm.box = "ubuntu/jammy64"
+    config.vm.box_check_update = false #deshabilitamos el update automatico dle box
+
+    (1..MasterCount).each do |i|
+      config.vm.define "master0#{i}" do |master|
+        master.vm.hostname = "master-node0#{i}"
+        master.vm.network "private_network", ip: IP_NW + "#{IP_START + i}"
+            master.vm.provider "virtualbox" do |vb|
+            vb.memory = 4048
+            vb.cpus = 2
+            vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+        end
+      #master.vm.provision "shell", path: "scripts/common.sh"
+      #master.vm.provision "shell", path: "scripts/master.sh"
+      end
+    end
+
+    (1..NodeCount).each do |i|
+      config.vm.define "node0#{i}" do |node|
+        node.vm.hostname = "worker-node0#{i}"
+        node.vm.network "private_network", ip: IP_NW + "#{IP_START + i}"
+        node.vm.provider "virtualbox" do |vb|
+            vb.memory = 2048
+            vb.cpus = 1
+            vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+        end
+        #node.vm.provision "shell", path: "scripts/common.sh"
+        #node.vm.provision "shell", path: "scripts/node.sh"
+      end
+    end
+  end
---
+++ script/common.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#Deshabilitamos swap
+sudo swapoff -a #desmontamos la memoria swap
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab #quitamos la swap del fstab
+
+#Configuramos modulos del sysctl
+#habilitamos overlay y br_netfilter
+sudo modprobe overlay 
+sudo modprobe br_netfilter
+
+# añadimos configuracion sysctl
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+# recargamos sysctl
+sudo sysctl --system
+
+#Instalamos kubernetes
+
+sudo apt update -y #Actualizamos repositorios
+
+sudo apt -y install curl apt-transport-https vim git curl wget #Instalamos paquetes necesarios
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - #Importamos key
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list #Añadimos repositorios de k8s
+
+sudo apt update #Actualizamos para que coja los repositorios nuevos
+sudo apt -y install kubelet kubeadm kubectl #Instalamos los paquetes de k8s
+sudo apt-mark hold kubelet kubeadm kubectl #bloqueamos la version de los paquetes
+
+#Instalamos CRI-O https://github.com/cri-o/cri-o/blob/main/tutorials/install-distro.md
+
+OS=xUbuntu_22.04
+CRIO_VERSION=1.18
+
+echo "deb [signed-by=/usr/share/keyrings/libcontainers-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+echo "deb [signed-by=/usr/share/keyrings/libcontainers-crio-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+
+sudo mkdir -p /usr/share/keyrings
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo gpg --dearmor -o /usr/share/keyrings/libcontainers-archive-keyring.gpg
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/Release.key | sudo gpg --dearmor -o /usr/share/keyrings/libcontainers-crio-archive-keyring.gpg
+
+sudo apt-get -y update
+sudo apt-get -y install cri-o
