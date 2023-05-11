778ad174bd1f897e998304f35123d60b030dc58e
b14nc0 <blanco88@gmail.com>
2023-05-11 13:59:31 +0200

    modified:   k8s/Vagrantfile

--- k8s/configs/join.sh
+++ k8s/configs/join.sh
@@ -1,1 +1,1 @@
-kubeadm join 10.0.0.10:6443 --token g6unlh.hl9l06jgav7lkxxu --discovery-token-ca-cert-hash sha256:395a543102eea533e3d30d29bd01d5cd177bdf1d8caea8e35b373f8312be3621 
+kubeadm join 10.0.0.10:6443 --token czuhen.wqeyy5vt5sl1fg2b --discovery-token-ca-cert-hash sha256:1fa543e444e34e0e89afb8a90bad77af1c5d331d3f74cc2c3bb8f96d2243b7aa 
