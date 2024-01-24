6c5a81ea062e24a122471520cc148d5c27049aa2
jblanco <jblanco@genmachine.b14nc0.local>
2024-01-24 12:56:21 +0100

    cluster k8s

--- k8s/README.md
+++
@@ -1,8 +0,0 @@
-# K8
-Deploy cluster k8s
-
-# Exportar kubeconfig
-<pre>
-cd configs
-**export KUBECONFIG=$(pwd)/config**
-</pre>
--- k8s/configs/join.sh
+++
@@ -1,1 +0,0 @@
-kubeadm join 10.0.0.10:6443 --token czuhen.wqeyy5vt5sl1fg2b --discovery-token-ca-cert-hash sha256:1fa543e444e34e0e89afb8a90bad77af1c5d331d3f74cc2c3bb8f96d2243b7aa 
